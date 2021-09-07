14fc365cd76990eb0979f8684f242651c15ff49e
Tilahun Assefa Demissie <til.assf98@gmail.com>
2021-09-07 11:05:37 -0500

    4.7 exercise class and object submit

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++
@@ -1,2 +0,0 @@
-package org.launchcode.java.demos.lsn3classes1;public class Course {
-}
---
+++ src/org/launchcode/java/demos/lsn3classes1/School/Course.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.demos.lsn3classes1.School;
+
+import org.launchcode.java.demos.lsn3classes1.Student;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String description;
+    private ArrayList<Student> students ;
+    private Teacher teacher;
+}
