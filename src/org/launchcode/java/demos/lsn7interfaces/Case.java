286312a8791a47e004753096e511ad68bfe72a1f
Tilahun Assefa Demissie <til.assf98@gmail.com>
2021-09-26 15:59:56 -0500

    8.6. Exercises sorting using Interfaces and Polymorphism done

--- src/org/launchcode/java/demos/lsn7interfaces/Case.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Case.java
@@ -2,16 +2,18 @@ package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Case {
     private ArrayList<Flavor> flavors = new ArrayList<>();
     private ArrayList<Cone> cones = new ArrayList<>();
+    private ArrayList<Topping> toppings= new ArrayList<>();
 
     public Case(){
         Cone cone1 = new Cone("Waffle", 1.25, new ArrayList<>(Arrays.asList( "gluten")));
         Cone cone2 = new Cone("Sugar", 0.75, new ArrayList<>(Arrays.asList( "gluten")));
         Cone cone3 = new Cone("Wafer", 0.50, new ArrayList<>(Arrays.asList( "gluten")));
-        Cone cone4 = new Cone("Bowl", 0.05, new ArrayList<>(Arrays.asList( "none")));
+        Cone cone4 = new Cone("Bowl", 0.05, new ArrayList<>(List.of("none")));
         cones.add(cone1);
         cones.add(cone2);
         cones.add(cone3);
@@ -27,6 +29,15 @@ public class Case {
         flavors.add(flavor3);
         flavors.add(flavor4);
         flavors.add(flavor5);
+
+        Topping hotFudge = new Topping("Hot Fudge", 2.14, new ArrayList<>(Arrays.asList("Sugar", "Nonfat Milk",  "Corn Syrup", "Soybean Oil", "Cocoa")));
+        Topping sprinkles = new Topping("Sprinkles", 1.14, new ArrayList<>(Arrays.asList("corn syrup", "sugar", "cornstarch", "wax", "artificial flavors", "colors")));
+        Topping caramel = new Topping("Caramel", .90, new ArrayList<>(Arrays.asList("Corn Syrup", "Sugar", "Nonfat Milk", "Soybean Oil")));
+        Topping oreos = new Topping("Oreos", 0.75, new ArrayList<>(Arrays.asList("Milk", "Heavy Cream", "White Sugar", "Salt", "Oreo Cookies")));
+        toppings.add(hotFudge);
+        toppings.add(sprinkles);
+        toppings.add(caramel);
+        toppings.add(oreos);
     }
 
     public ArrayList<Flavor> getFlavors() {
@@ -43,5 +54,13 @@ public class Case {
 
     public void setCones(ArrayList<Cone> cones) {
         this.cones = cones;
+    }
+
+    public ArrayList<Topping> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(ArrayList<Topping> toppings) {
+        this.toppings = toppings;
     }
 }
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -7,13 +7,25 @@ public class Main {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        ArrayList<Topping> toppings = menu.getToppings();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
-
+flavors.sort(new FlavorComparator());
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
-
+cones.sort(new ConeComparator());
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        for (Flavor flavor: flavors){
+            System.out.println(flavor.toString());
+        }
+        for (Cone cone: cones){
+            System.out.println(cone.toString());
+        }
+
+        toppings.sort(new ToppingComparator());
+        for (Topping topping: toppings){
+            System.out.println(topping);
+        }
     }
 }
